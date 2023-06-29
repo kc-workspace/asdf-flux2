ad8075c3b8710349ac07f11d8e0e2cab78a409bb
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-06-29 22:37:12 +0700

    feat(core): applied template

--- lib/common/index.sh
+++ lib/common/index.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
-
-## Environment variables
+#!/usr/bin/env bash## Environment variables
 ## https://asdf-vm.com/plugins/create.html#environment-variables-overview
 
-# shellcheck source-path=SCRIPTDIR/defaults.sh
-source "${KC_ASDF_PLUGIN_PATH:?}/lib/common/defaults.sh"
 # shellcheck source-path=SCRIPTDIR/internal.sh
 source "${KC_ASDF_PLUGIN_PATH:?}/lib/common/internal.sh"
+# shellcheck source-path=SCRIPTDIR/defaults.sh
+source "${KC_ASDF_PLUGIN_PATH:?}/lib/common/defaults.sh"
+# shellcheck source-path=SCRIPTDIR/main.sh
+source "${KC_ASDF_PLUGIN_PATH:?}/lib/common/main.sh"
 
 ## System information
 KC_ASDF_OS="$(kc_asdf_get_os)"
@@ -17,20 +17,13 @@ KC_ASDF_NAME="asdf-flux2"
 KC_ASDF_REPO="https://github.com/kc-workspace/asdf-flux2"
 ## Application information
 KC_ASDF_APP_NAME="flux"
-KC_ASDF_APP_DESC="open and extensible continuous delivery solution for Kubernetes."
+KC_ASDF_APP_DESC=""
 KC_ASDF_APP_REPO="https://github.com/fluxcd/flux2"
-## Download settings
-KC_ASDF_DOWNLOAD_URL="https://github.com/fluxcd/flux2/releases/download/v{version}/flux_{version}_{os}_{arch}.tar.gz"
-KC_ASDF_DOWNLOAD_NAME="flux_{version}_{os}_{arch}.tar.gz"
-KC_ASDF_CHECKSUM_URL="https://github.com/fluxcd/flux2/releases/download/v{version}/flux_{version}_checksums.txt"
-KC_ASDF_DOWNLOAD_LOC="flux"
 
 ## These are set on bin/* scripts
 # export KC_ASDF_PLUGIN_ENTRY_PATH
 # export KC_ASDF_PLUGIN_ENTRY_NAME
 # export KC_ASDF_PLUGIN_PATH
-export KC_ASDF_APP_NAME KC_ASDF_APP_DESC KC_ASDF_APP_REPO
-export KC_ASDF_DOWNLOAD_URL KC_ASDF_DOWNLOAD_NAME KC_ASDF_DOWNLOAD_LOC
-export KC_ASDF_CHECKSUM_URL
-export KC_ASDF_ORG KC_ASDF_NAME KC_ASDF_REPO
 export KC_ASDF_OS KC_ASDF_ARCH
+export KC_ASDF_ORG KC_ASDF_NAME KC_ASDF_REPO
+export KC_ASDF_APP_NAME KC_ASDF_APP_DESC KC_ASDF_APP_REPO
